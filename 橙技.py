52b3b41d34848037f5a0163e1a718436254894d3
jiayezi <1601235906@qq.com>
2023-03-05 20:39:15 +0800

    优化代码

--- 橙技.py
+++ 橙技.py
@@ -13,15 +13,15 @@ from win32com import client
 
 def initialize():
     """释放文本框，清空文本框"""
-    text3.config(state=NORMAL)
-    text2.delete(1.0, END)  # 删除文本框里的内容
-    text3.delete(1.0, END)
+    info_text.config(state=NORMAL)
+    output_text.delete(1.0, END)  # 删除文本框里的内容
+    info_text.delete(1.0, END)
 
 
 def timu():
     """提取数字"""
     initialize()
-    data = text1.get(1.0, END)
+    data = input_text.get(1.0, END)
     data = data.strip()
     if data:
         data_list = data.split('\n')
@@ -29,14 +29,14 @@ def timu():
         for i, s in enumerate(data_list):
             data_obj = search(r'\d{1,2}', s)
             if not data_obj:
-                text3.insert('end', '没有找到数字\n')
+                info_text.insert('end', '没有找到数字\n')
                 over()
                 return
             data = data_obj.group()
             text += f'{data}\n'
-        text2.insert('end', text)
-        text3.insert('end', '提取完成\n')
-        text2.focus()
+        output_text.insert('end', text)
+        info_text.insert('end', '提取完成\n')
+        output_text.focus()
     over()
 
 
@@ -45,7 +45,7 @@ def nandu():
     initialize()
     counter = 0
 
-    data = text1.get(1.0, END)
+    data = input_text.get(1.0, END)
     data = data.strip()
 
     if data:
@@ -58,21 +58,21 @@ def nandu():
                 num *= 100
             except ValueError:
                 text += '\n'
-                text3.insert('end', f'第 {i + 1} 行不是纯数字，处理失败\n')
+                info_text.insert('end', f'第 {i + 1} 行不是纯数字，处理失败\n')
             else:
                 text += f'{str(int(num))}\n'
                 counter += 1
 
-        text2.insert('end', text[:-1])
-        text3.insert('end', f'处理了 {counter} 个难度值\n')
-        text2.focus()
+        output_text.insert('end', text[:-1])
+        info_text.insert('end', f'处理了 {counter} 个难度值\n')
+        output_text.focus()
     over()
 
 
 def xuanzeti():
     """提取字符串里的A、B、C、D、E、F、G"""
     initialize()
-    data = text1.get(1.0, END)  # 获取文本框里的数据
+    data = input_text.get(1.0, END)  # 获取文本框里的数据
     data = data.strip()
     if data:
         counter = 0
@@ -81,9 +81,9 @@ def xuanzeti():
             if s in ('A', 'B', 'C', 'D', 'E', 'F', 'G'):
                 text += f'{s}\n'
                 counter += 1
-        text2.insert('end', text[:-1])
-        text3.insert('end', f'提取了 {counter} 个答案\n')
-        text2.focus()
+        output_text.insert('end', text[:-1])
+        info_text.insert('end', f'提取了 {counter} 个答案\n')
+        output_text.focus()
     over()
 
 
@@ -91,7 +91,7 @@ def nengli():
     """把符号替换成该列对应的文字"""
     initialize()
 
-    data = text1.get(1.0, END)
+    data = input_text.get(1.0, END)
     data = data.strip()
     data_list = data.split('\n')
 
@@ -109,11 +109,11 @@ def nengli():
                     break
             else:
                 text += f'\n'
-                text3.insert('end', f'第 {row_index+2} 行没有符号\n')
-        text2.insert('end', text[:-1])
+                info_text.insert('end', f'第 {row_index + 2} 行没有符号\n')
+        output_text.insert('end', text[:-1])
 
-        text3.insert('end', '全部处理完成\n')
-        text2.focus()
+        info_text.insert('end', '全部处理完成\n')
+        output_text.focus()
     over()
 
 
@@ -121,7 +121,7 @@ def omr():
     """删除制表符，把长度不是1的字符串替换成."""
     initialize()
 
-    data = text1.get(1.0, END)
+    data = input_text.get(1.0, END)
     data = data.strip().replace(' ', '.')
     data_list = data.split('\n')
 
@@ -130,14 +130,14 @@ def omr():
         line_list = line.split('\t')
         for s in line_list:
             if len(s) == 1:
-                text2.insert('end', f'{s}')
+                output_text.insert('end', f'{s}')
             else:
-                text2.insert('end', '.')
+                output_text.insert('end', '.')
                 counter += 1
-        text2.insert('end', '\n')
+        output_text.insert('end', '\n')
 
-    text3.insert('end', f'替换 {counter} 处多选\n')
-    text2.focus()
+    info_text.insert('end', f'替换 {counter} 处多选\n')
+    output_text.focus()
     over()
 
 
@@ -145,7 +145,7 @@ def buding():
     """合并不定向选择答案"""
     initialize()
 
-    data = text1.get(1.0, END)
+    data = input_text.get(1.0, END)
     data = data.strip().replace(' ', '.')
     data_list = data.split('\n')
 
@@ -153,12 +153,12 @@ def buding():
         line_list = line.split('\t')
         for s in line_list:
             if len(s) > 0:
-                text2.insert('end', f'[{s}]')
+                output_text.insert('end', f'[{s}]')
             else:
-                text2.insert('end', '[.]')
-        text2.insert('end', '\n')
+                output_text.insert('end', '[.]')
+        output_text.insert('end', '\n')
 
-    text2.focus()
+    output_text.focus()
     over()
 
 
@@ -195,9 +195,9 @@ def xiaofen():
         ws.Cells(max_row + 2, 3).Value = f'=MAX(C2:C{max_row})'
 
         if ws.Cells(max_row + 1, 3).Value == ws.Cells(max_row + 2, 3).Value:
-            text3.insert(END, f'选择题答案数量一样\n')
+            info_text.insert(END, f'选择题答案数量一样\n')
         else:
-            text3.insert(END, '选择题答案数量不一样，请检查这个科目是否有多选题 Σ(ŎдŎ|||)ﾉﾉ\n')
+            info_text.insert(END, '选择题答案数量不一样，请检查这个科目是否有多选题 Σ(ŎдŎ|||)ﾉﾉ\n')
         ws.Cells(max_row + 1, 3).Value = None
         ws.Cells(max_row + 2, 3).Value = None
         ws.Columns(3).Delete()
@@ -210,9 +210,9 @@ def xiaofen():
             col_name = ws.Cells(max_row + 1, col).Value
             # 计算最大值
             ws.Cells(max_row + 2, col).Value = f'=MAX({col_name}2:{col_name}{max_row})'
-            text2.insert(END, f'{ws.Cells(max_row + 2, col).Value}\n')
-        text3.insert('end', '题目最高分查找完成\n')
-        text3.tag_add('forever', 1.0, END)
+            output_text.insert(END, f'{ws.Cells(max_row + 2, col).Value}\n')
+        info_text.insert('end', '题目最高分查找完成\n')
+        info_text.tag_add('forever', 1.0, END)
         # 删除2行临时数据
         ws.Rows(max_row + 1).Delete()
         ws.Rows(max_row + 1).Delete()
@@ -237,12 +237,12 @@ def xiaofen():
         wb.Close(SaveChanges=1)  # 保存并关闭
         excel.Quit()
 
-        text3.insert(END, '小分表修改完成\n')
-        text3.tag_add('forever', 1.0, END)
-        text3.yview_moveto(1)
-        text2.focus()
+        info_text.insert(END, '小分表修改完成\n')
+        info_text.tag_add('forever', 1.0, END)
+        info_text.yview_moveto(1)
+        output_text.focus()
     else:
-        text3.insert('end', '没有打开Excel文件\n')
+        info_text.insert('end', '没有打开Excel文件\n')
 
     over()
 
@@ -277,16 +277,16 @@ def total_score():
                         student_dict[student_id] = [0.0 for _ in range(counter + 1)]
                     student_dict[student_id][counter] = score
             except IndexError:
-                text3.insert(END, '数据不完整，处理失败，请同时提交考号和成绩 (ー_ー)!!\n')
+                info_text.insert(END, '数据不完整，处理失败，请同时提交考号和成绩 (ー_ー)!!\n')
                 break
             except ValueError:
-                text3.insert(END, '成绩字段不是纯数字，处理失败 (ー_ー)!!\n')
+                info_text.insert(END, '成绩字段不是纯数字，处理失败 (ー_ー)!!\n')
                 break
 
             counter += 1
-            text3.insert(END, f'已提交 {counter} 个科目成绩\n')
-            text3.tag_add('forever', 1.0, END)
-            text3.yview_moveto(1)
+            info_text.insert(END, f'已提交 {counter} 个科目成绩\n')
+            info_text.tag_add('forever', 1.0, END)
+            info_text.yview_moveto(1)
 
             choice = messagebox.askyesno('添加确认', '是否继续添加其他科目成绩？')
             if choice:
@@ -296,7 +296,7 @@ def total_score():
             else:
                 break
         else:
-            text3.insert(END, '没有输入考号和分数\n')
+            info_text.insert(END, '没有输入考号和分数\n')
             break
 
     titles.append('总分')
@@ -325,7 +325,7 @@ def total_score():
                                                  defaultextension='.xlsx')
         if file_path:
             wb.save(file_path)
-            text3.insert(END, '文件保存成功\n')
+            info_text.insert(END, '文件保存成功\n')
             wb.close()
     over()
 
@@ -334,7 +334,7 @@ def chaifen():
     """按小题的分数拆分总分"""
     initialize()
 
-    total = text1.get(1.0, END)
+    total = input_text.get(1.0, END)
     total = total.strip()
     total_list = total.split('\n')
 
@@ -350,35 +350,35 @@ def chaifen():
                 for score in score_list:
                     num_score = float(score)
                     if num_total >= num_score:
-                        text2.insert('end', f'{score}\t')
+                        output_text.insert('end', f'{score}\t')
                         num_total -= num_score
                     else:
-                        text2.insert('end', f'{str(num_total)}\t')
+                        output_text.insert('end', f'{str(num_total)}\t')
                         num_total = 0
                 counter += 1
-                text2.insert('end', '\n')
+                output_text.insert('end', '\n')
         except ValueError:
-            text3.insert('end', '总分或题目分不是纯数字，拆分失败 (ー_ー)!!\n')
+            info_text.insert('end', '总分或题目分不是纯数字，拆分失败 (ー_ー)!!\n')
         else:
-            text3.insert('end', f'拆散 {counter} 个总分\n')
-            text2.focus()
+            info_text.insert('end', f'拆散 {counter} 个总分\n')
+            output_text.focus()
     else:
-        text3.insert('end', '没有输入题目分数\n')
+        info_text.insert('end', '没有输入题目分数\n')
 
     over()
 
 
 def over():
     """改变文本颜色，禁用文本框"""
-    text3.tag_add('forever', 1.0, END)
-    text3.config(state=DISABLED)
-    text3.yview_moveto(1)  # 文本更新滚动显示
+    info_text.tag_add('forever', 1.0, END)
+    info_text.config(state=DISABLED)
+    info_text.yview_moveto(1)  # 文本更新滚动显示
 
 
 def show_message():
     top = ttk.Toplevel()
     top.title('软件介绍')
-    top.geometry('600x320+680+300')  # 窗口大小
+    top.geometry(f'600x320+{offset_x}+{offset_y}')  # 窗口大小
     top.maxsize(700, 400)
     top.minsize(500, 200)
 
@@ -406,14 +406,14 @@ def about():
 
 
 def select_all(event):
-    text3.config(state=NORMAL)
-    text3.insert(END, '选中全部\n')
+    info_text.config(state=NORMAL)
+    info_text.insert(END, '选中全部\n')
     over()
 
 
 def cp_msg(event):
-    text3.config(state=NORMAL)
-    text3.insert(END, '已复制到剪贴板\n')
+    info_text.config(state=NORMAL)
+    info_text.insert(END, '已复制到剪贴板\n')
     over()
 
 
@@ -424,8 +424,13 @@ def close_handle():
 
 # 窗口
 root = ttk.Window(themename='cerculean', title='橙技')
-root.geometry('1080x800+450+100')  # 窗口大小
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+offset_x = int((screen_width-1080)/2)
+offset_y = int((screen_height-800)/2)
+root.geometry(f'1080x800+{offset_x}+{offset_y}')  # 窗口大小
 root.minsize(1000, 750)
+root.iconbitmap('green_apple.ico')
 
 # 菜单
 menubar = ttk.Menu(root)
@@ -434,62 +439,62 @@ help_menu.add_command(label='介绍', command=show_message)
 help_menu.add_command(label='关于', command=about)
 menubar.add_cascade(label='帮助', menu=help_menu)
 
-label1 = ttk.Label(root, text='原始数据', font=('黑体', 12), width=10)
+label1 = ttk.Label(root, text='原始数据', font=('黑体', 12))
 label1.pack(pady=10, side=TOP)  # 按布局方式放置标签
 
-text1 = ttk.Text(root, width=120, height=14)
-text1.pack()
-text1.focus()
+input_text = ttk.Text(root, width=120, height=13)
+input_text.pack()
+input_text.focus()
 
-label2 = ttk.Label(root, text='处理结果', font=('黑体', 12), width=10)
+label2 = ttk.Label(root, text='计算结果', font=('黑体', 12))
 label2.pack(pady=10, side=TOP)
 
-text2 = ttk.Text(root, width=120, height=14)
-text2.pack()
-text2.bind('<Control-a>', select_all)  # 绑定事件
-text2.bind('<Control-A>', select_all)
-text2.bind('<Control-c>', cp_msg)
-text2.bind('<Control-C>', cp_msg)
-text2.bind('<Control-x>', cp_msg)
-text2.bind('<Control-X>', cp_msg)
+output_text = ttk.Text(root, width=120, height=13)
+output_text.pack()
+output_text.bind('<Control-a>', select_all)  # 绑定事件
+output_text.bind('<Control-A>', select_all)
+output_text.bind('<Control-c>', cp_msg)
+output_text.bind('<Control-C>', cp_msg)
+output_text.bind('<Control-x>', cp_msg)
+output_text.bind('<Control-X>', cp_msg)
 
-text3 = ttk.Text(root, width=120, height=6, border=-1)
-text3.pack(pady=10, side=TOP)
-text3.tag_config('forever', foreground='green', font=('黑体', 11), spacing3=5)
-text3.config(state=DISABLED)
+info_text = ttk.Text(root, width=120, height=6, border=-1)
+info_text.pack(pady=10, side=TOP)
+info_text.tag_config('forever', foreground='green', font=('黑体', 11), spacing3=5)
+info_text.config(state=DISABLED)
 
 # 按钮区域
 buttonbar = ttk.Frame(root)
-buttonbar.pack(padx=10, pady=20, side=BOTTOM)
+buttonbar.pack(pady=10, side=TOP)
 
 btn = ttk.Button(master=buttonbar, text='题目', compound=LEFT, command=timu)
-btn.pack(side=LEFT, ipadx=15, padx=10, pady=5)
+btn.pack(side=LEFT, ipadx=15, padx=10)
 
 btn = ttk.Button(master=buttonbar, text='难度值', compound=LEFT, command=nandu)
-btn.pack(side=LEFT, ipadx=15, padx=10, pady=5)
+btn.pack(side=LEFT, ipadx=15, padx=10)
 
 btn = ttk.Button(master=buttonbar, text='单选答案', compound=LEFT, command=xuanzeti)
-btn.pack(side=LEFT, ipadx=15, padx=10, pady=5)
+btn.pack(side=LEFT, ipadx=15, padx=10)
 
 btn = ttk.Button(master=buttonbar, text='能力要求', compound=LEFT, command=nengli)
-btn.pack(side=LEFT, ipadx=15, padx=10, pady=5)
+btn.pack(side=LEFT, ipadx=15, padx=10)
 
 btn = ttk.Button(master=buttonbar, text='OMR', compound=LEFT, command=omr)
-btn.pack(side=LEFT, ipadx=15, padx=10, pady=5)
+btn.pack(side=LEFT, ipadx=15, padx=10)
 
 btn = ttk.Button(master=buttonbar, text='多选OMR', compound=LEFT, command=buding)
-btn.pack(side=LEFT, ipadx=15, padx=10, pady=5)
+btn.pack(side=LEFT, ipadx=15, padx=10)
 
 btn = ttk.Button(master=buttonbar, text='小分表', compound=LEFT, command=xiaofen)
-btn.pack(side=LEFT, ipadx=15, padx=10, pady=5)
+btn.pack(side=LEFT, ipadx=15, padx=10)
 
 btn = ttk.Button(master=buttonbar, text='总分表', compound=LEFT, command=total_score)
-btn.pack(side=LEFT, ipadx=15, padx=10, pady=5)
+btn.pack(side=LEFT, ipadx=15, padx=10)
 
 btn = ttk.Button(master=buttonbar, text='拆分', compound=LEFT, command=chaifen)
-btn.pack(side=LEFT, ipadx=15, padx=10, pady=5)
+btn.pack(side=LEFT, ipadx=15, padx=10)
 
-root.protocol('WM_DELETE_WINDOW', close_handle)  # 点击关闭按钮，触发事件
+root.protocol('WM_DELETE_WINDOW', close_handle)  # 启用协议处理机制，点击关闭时按钮，触发事件
 
 root.config(menu=menubar)
 root.mainloop()
