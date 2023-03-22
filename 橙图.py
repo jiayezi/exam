b7956dee765ec3081de2322b3a43cbddee7e501d
jiayezi <1601235906@qq.com>
2023-03-22 18:26:36 +0800

    正在添加新功能

--- 橙图.py
+++ 橙图.py
@@ -11,6 +11,8 @@ from ttkbootstrap.constants import *
 from win32com import client  # 操作office文档，转换格式
 
 
+# todo : 图片裁剪、制作选择题时选择起始数字
+
 def initialize():
     """取消冻结文本框，清空文本框"""
     info_text.config(state=NORMAL)
