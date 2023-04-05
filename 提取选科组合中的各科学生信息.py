c049a01cebbab69432165a2bbadd2989250e108b
jiayezi <1601235906@qq.com>
2023-04-05 14:43:26 +0800

    添加计算每个考场人数的功能

--- 提取选科组合中的各科学生信息.py
+++ 提取选科组合中的各科学生信息.py
@@ -2,7 +2,7 @@ from tkinter import filedialog
 from openpyxl import Workbook, load_workbook
 
 
-# 科目范围
+# 科目范围，是列数，不是索引
 subject_range = (1, 3)
 
 
@@ -37,6 +37,29 @@ def get_student_info():
             ws.append(row)
         wb.save(f'{save_dir}/{subject_name}.xlsx')
         wb.close()
+
+    # 计算每个考场的人数
+    room_index = 5
+    wb = Workbook()
+    ws = wb.active
+    ws.append(('科目', '考室', '人数'))
+    for subject_name in subject_dict.keys():
+        student_list = subject_dict[subject_name]
+        exam_room_dict = {}
+        for row in student_list:
+            room_name = row[room_index]
+            if room_name in exam_room_dict.keys():
+                exam_room_dict[room_name] += 1
+            else:
+                exam_room_dict[room_name] = 1
+        counter = 0
+        for k, v in exam_room_dict.items():
+            ws.append((subject_name, f'第{k}考室', v))
+            counter += v
+        ws.append((subject_name, '加印', 10))
+        ws.append((subject_name, '总计', counter+10))
+        ws.append([])
+    wb.save(f'{save_dir}/印刷安排.xlsx')
 
 
 if __name__ == '__main__':
