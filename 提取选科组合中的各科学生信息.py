7586fbac2ad3281f348d78faa2554f59d1ee8417
jiayezi <1601235906@qq.com>
2023-11-09 16:31:03 +0800

    添加计算语文数学英语科目的每个考室的人数

--- 提取选科组合中的各科学生信息.py
+++ 提取选科组合中的各科学生信息.py
@@ -2,49 +2,53 @@ from tkinter import filedialog
 from openpyxl import Workbook, load_workbook
 
 
-# 科目范围，是列数，不是索引
-subject_range = (3, 5)
+# 选科名称的索引
+subject_range = (2, 3, 4)
+# 考室号的索引
+room_index = 5
+# 主科
+pub_subject = '语文、数学、英语'
 
 
 def get_student_info():
-    # 提取
     file_path = filedialog.askopenfilename(title='选择Excel文件', filetypes=[('Excel', '.xlsx')],
                                            defaultextension='.xlsx')
     if not file_path:
         return
-    subject_dict = {'政治': [], '历史': [], '地理': [], '物理': [], '化学': [], '生物': []}
+    subject_dict = {pub_subject: [], '政治': [], '历史': [], '地理': [], '物理': [], '化学': [], '生物': []}
     wb = load_workbook(file_path)
     ws = wb.active
     title = next(ws.values)
-    for col in range(subject_range[0]-1, subject_range[1]):
-        for i, row in enumerate(ws.values):
-            if i == 0:
-                continue
-            subject_name = row[col]
+    # 提取每个科目的学生信息
+    for i, row in enumerate(ws.values):
+        if i == 0:
+            continue
+        subject_dict[pub_subject].append(row)
+        for j in range(3):
+            subject_name = row[subject_range[j]]
             subject_dict[subject_name].append(row)
     wb.close()
 
-    # 保存
+    # 分科目保存
     save_dir = filedialog.askdirectory(title='选择存储文件夹', initialdir='F:/用户目录/桌面/')
     if not save_dir:
         return
-    for subject_name in subject_dict.keys():
+    for subject_name,  student_list in subject_dict.items():
+        if subject_name == pub_subject:
+            continue
         wb = Workbook()
         ws = wb.active
         ws.append(title)
-        student_list = subject_dict[subject_name]
         for row in student_list:
             ws.append(row)
         wb.save(f'{save_dir}/{subject_name}.xlsx')
         wb.close()
 
     # 计算每个考场的人数
-    room_index = 5
     wb = Workbook()
     ws = wb.active
     ws.append(('科目', '考室', '人数'))
-    for subject_name in subject_dict.keys():
-        student_list = subject_dict[subject_name]
+    for subject_name, student_list in subject_dict.items():
         exam_room_dict = {}
         for row in student_list:
             room_name = row[room_index]
