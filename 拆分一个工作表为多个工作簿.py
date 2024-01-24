d01ff35fb98b3c11dcc539eebe6df07d64e8ff40
jiayezi <1601235906@qq.com>
2024-01-24 18:18:43 +0800

    简单优化

--- 拆分一个工作表为多个工作簿.py
+++ 拆分一个工作表为多个工作簿.py
@@ -1,26 +1,30 @@
 from tkinter import filedialog
 import openpyxl
 
-# 指定拆分的列和存储目录
-name_index = 1
+
+# 指定拆分的列
+name_index = 3
+# 标题行数
+title_rows = 1
+# 存储目录
 save_path = 'F:/用户目录/桌面/全部学校'
 
 path = filedialog.askopenfilename(title='请选择Excel文件', initialdir='F:/用户目录/桌面/',
                                   filetypes=[('Excel', '.xlsx')], defaultextension='.xlsx')
 
-wb = openpyxl.load_workbook(path)
+wb = openpyxl.load_workbook(path, read_only=True)
 ws = wb.active
 
 # 获取全部名字
 names = set()
 for i, row in enumerate(ws.values):
-    if i == 0:
+    if i < title_rows:
         continue
     names.add(row[name_index])
 
 # 提取数据
 for name in names:
-    wb_new = openpyxl.Workbook()
+    wb_new = openpyxl.Workbook(write_only=True)
 
     # 根据原始工作簿的工作表创建新工作簿的工作表，然后添加数据
     for ws in wb:
@@ -28,14 +32,14 @@ for name in names:
         # ws_new = wb_new.active
         for i, row in enumerate(ws.values):
             # 添加标题
-            if i < 2:
+            if i < title_rows:
                 ws_new.append(row)
             # 添加数据
             if row[name_index] == name:
                 ws_new.append(row)
         # 合并单元格
-        for rg in ws.merged_cells:
-            ws_new.merge_cells(str(rg))
+        # for rg in ws.merged_cells:
+        #     ws_new.merge_cells(str(rg))
 
     # 保存并关闭工作簿
     wb_new.save(f'{save_path}/{name}.xlsx')
